abd6a913849348d095a19310f116616e91c84cf1
han <yhan@la-francaise.com>
2018-12-06 11:58:49 +0100

    init repo

---
+++ Caraibes.EventStore.TestHarness/EventStoreSubscriberProgram.cs
@@ -0,0 +1,55 @@
+﻿namespace Caraibes.EventStore.TestHarness
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Caraibes.Domain;
+    using Caraibes.Domain.EventSourcing;
+    using Caraibes.Infrastructure;
+    using Caraibes.Messaging;
+
+    class EventStoreSubscriberProgram
+    {
+        static void Main(string[] args)
+        {
+
+            Task.Run(Execute).Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task Execute()
+        {
+            var connectionFactory = new EventStoreConnectionFactory("ConnectTo=tcp://127.0.0.1:1113;");
+            var bus = new EventStoreBus(connectionFactory);
+            var canUnsubscribe = bus.RegisterHandler(new EventSubscriptionRequest<Event>(OnEventReceived, "$ce-hello", lastProcessedEventStreamPosition: null));
+
+            var eventStore = new EventStore(connectionFactory, null);
+
+            var streamName = StreamNameBuilder.BuildStreamName<Hello>(Guid.NewGuid());
+            await eventStore.CreateNewStream(streamName, new Event[] { new HelloEvent(),  });
+            
+            bool keepContinue = true;
+            while (keepContinue)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.RightArrow:
+
+                        var lastExpectedEventVersion = eventStore.GetStreamLastExpectedEventVersion(streamName);
+                        await eventStore.AppendEventsToStream( streamName, new[]  { new HelloEvent(),  }, lastExpectedEventVersion);
+
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        keepContinue = false;
+                        break;
+                }
+            }
+        }
+
+        private static void OnEventReceived(Event evt)
+        {
+            Console.WriteLine($"Event {evt.LinkEventNumber} arrived");
+        }
+    }
+}
---
+++ Caraibes.EventStore.Write.TestHarness/EventStoreWriterProgram.cs
@@ -0,0 +1,46 @@
+﻿namespace Caraibes.EventStore.Write.TestHarness
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Caraibes.Domain;
+    using Caraibes.Domain.EventSourcing;
+    using Caraibes.Infrastructure;
+
+    class EventStoreWriterProgram
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(Execute).Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task Execute()
+        {
+            var connectionFactory = new EventStoreConnectionFactory("ConnectTo=tcp://127.0.0.1:1113;");
+
+            var eventStore = new EventStore(connectionFactory, null);
+
+            var streamName = StreamNameBuilder.BuildStreamName<Hello>(Guid.NewGuid());
+            await eventStore.CreateNewStream(streamName, new Event[] { new HelloEvent() });
+            
+            bool keepContinue = true;
+            while (keepContinue)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.RightArrow:
+
+                        var lastExpectedEventVersion = eventStore.GetStreamLastExpectedEventVersion(streamName);
+                        await eventStore.AppendEventsToStream(streamName, new[] { new HelloEvent() }, lastExpectedEventVersion);
+
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        keepContinue = false;
+                        break;
+                }
+            }
+        }
+    }
+}
